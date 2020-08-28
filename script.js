b6410880fec214b6656d09c5efa4cedce12a5a43
Rebeca Vital Fernandes <44797156+rebvital@users.noreply.github.com>
2020-08-28 18:49:51 +0100

    docs

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function carry () {
+    let msg = document.getElementById('msg');
+    let pic = window.document.getElementById('img');
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes()
+    msg.innerHTML = `Now it's ${hour} hours and ${min} minutes. `
+    if (hour >= 4 && hour < 12){
+        msg.innerHTML += '<p>Good Morning!</p>';
+        pic.innerHTML = '<img src="morning.jpg" alt="morning"></img>'
+        document.body.style.background = '#e2cd9f';
+    } else if (hour >= 12 && hour < 20 ) {
+        msg.innerHTML += '<p>Good Afternoon!</p>';
+        pic.innerHTML = '<img src="afternoon.jpg" alt="morning"></img>';
+        document.body.style.background = '#b9846f'
+    } else {
+        msg.innerHTML += '<p>Good Night!</p>';
+        pic.innerHTML = '<img src="night.jpg" alt="morning"></img>'
+        document.body.style.background = '#515154'
+    }
+}
