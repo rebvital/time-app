f8e2e19ab38ad1a560781114a72544cd4057e8b4
Rebeca Vital Fernandes <44797156+rebvital@users.noreply.github.com>
2020-09-17 00:19:38 +0100

    updating repository

--- project/script.js
+++ project/script.js
@@ -9,15 +9,15 @@ function carry () {
     msg.innerHTML = `Now it's ${hour} hours and ${min} minutes. `
     if (hour >= 4 && hour < 12){
         msg.innerHTML += '<p>Good Morning!</p>';
-        pic.innerHTML = '<img src="morning.jpg" alt="morning"></img>'
+        pic.innerHTML = '<img src="../assets/morning.jpg" alt="morning"></img>'
         document.body.style.background = '#e2cd9f';
     } else if (hour >= 12 && hour < 20 ) {
         msg.innerHTML += '<p>Good Afternoon!</p>';
-        pic.innerHTML = '<img src="afternoon.jpg" alt="morning"></img>';
+        pic.innerHTML = '<img src="../assets/afternoon.jpg" alt="morning"></img>';
         document.body.style.background = '#b9846f'
     } else {
         msg.innerHTML += '<p>Good Night!</p>';
-        pic.innerHTML = '<img src="night.jpg" alt="morning"></img>'
+        pic.innerHTML = '<img src="../assets/night.jpg" alt="morning"></img>'
         document.body.style.background = '#515154'
     }
 }
